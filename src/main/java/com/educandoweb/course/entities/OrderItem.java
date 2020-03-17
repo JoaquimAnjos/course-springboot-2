ad99ab9c673211fd70f5eefbc4028ca34517e5e4
JoaquimAnjos <quinzinhodosanjos@gmail.com>
2020-03-17 16:33:22 +0000

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -63,6 +63,10 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {//para aparecer no json é necessário colocar get
+		return price * quantity;
+	}
 
 	@Override
 	public int hashCode() {
