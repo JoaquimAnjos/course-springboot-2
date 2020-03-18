e1ed3149afab396e50e407b824d91e3dc0eb9026
JoaquimAnjos <quinzinhodosanjos@gmail.com>
2020-03-18 16:19:34 +0000

    User insert

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) { //inserir na BD um novo objeto do tipo User
+		return repository.save(obj);
+	}
 }
