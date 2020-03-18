6cae1ca9691f6f7b0102fbfe34c1ba7dd1c4fc17
JoaquimAnjos <quinzinhodosanjos@gmail.com>
2020-03-18 17:08:38 +0000

    User update

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +50,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();// código http para uma resposta sem conteúdo é o 204
 	}
 	
-	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id,  obj);
+		return ResponseEntity.ok().body(obj);
+	}
 }
